3f5deaf50d7661b7fb6f79d49dd535d8a9105428
asouvanlasy <asouvanlasy@outlook.com>
2022-02-06 11:44:39 -0500

    Added arrays for each phrase

--- js/app.js
+++ js/app.js
@@ -8,6 +8,11 @@ var synth = window.speechSynthesis;
 var textToSpeak = 'This is the text string that you will generate with your script';
 var speakButton = document.querySelector('button');
 
+const nouns = ["The turkey","Mom","Dad","The dog","My teacher","The elephant","The cat"]; 
+const verbs = ["sat on","ate","danced with","saw","doesn't like","kissed"];
+const adjectives = ["a funny","a scary","a goofy","a slimy","a barking","a fat"]
+const animals = ["goat","monkey","fish","cow","frog","bug","worm"];
+const places = ["on the moon","on the chair","in my spaghetti","in my soup","on the grass","in my shoes"];
 
 /* Functions
 -------------------------------------------------- */
